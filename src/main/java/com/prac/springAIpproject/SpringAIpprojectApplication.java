f339336fa64de9c24dbac0893f4489f71b6cf4e5
ompratyushraj <ompratyushraj02@gmail.com>
2025-08-18 17:05:57 +0530

    Initial commit

---
+++ src/main/java/com/prac/springAIpproject/SpringAIpprojectApplication.java
@@ -0,0 +1,13 @@
+package com.prac.springAIpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIpprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAIpprojectApplication.class, args);
+	}
+
+}
